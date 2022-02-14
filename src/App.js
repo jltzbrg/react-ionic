486838dd9a2f1a34fab29d9cd689411c5f14c1af
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-02-14 23:42:58 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { IonButton, IonIcon, IonToast } from '@ionic/react'
+import { play as playIcon } from 'ionicons/icons'
+import React, { useState } from 'react'
+
+function App() {
+  const [showToast, setShowToast] = useState(false)
+  const handleClick = () => {
+    setShowToast(true)
+    setTimeout(() => setShowToast(false), 1500)
+  }
+  return (
+    <div>
+      <header>
+        <h1>My App</h1>
+      </header>
+      <main>
+        <IonButton onClick={handleClick}>
+          <IonIcon icon={playIcon} slot='start' />
+          Click Me
+        </IonButton>
+        <IonToast isOpen={showToast} message='Hola Mundo' />
+      </main>
+    </div>
+  )
+}
+
+export default App
